6aca9e8aa7b5ccea2e0461e36cfc959ec2177ea0
GeckoGamer47 <geck0.gam3r47@gmail.com>
2025-12-01 20:02:10 -0500

    started the dodge code

--- player_movement.gd
+++ player_movement.gd
@@ -1,11 +1,40 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var dodge_speed = 1000
+@export var dodge_time = 1
+@export var is_dodging = false
+@export var input_direction = Vector2.ZERO
+@export var dodge_direction=input_direction
+
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	if Input.is_action_just_pressed('ability'):
+		dodge()
+	if not is_dodging:
+		input_direction = Input.get_vector("left", "right", "up", "down")
+		velocity = input_direction * speed
+		
+func dodge():
+	print('enter dodge')
+	print(dodge_direction)
+	
+	is_dodging=true
+	
+	dodge_direction = input_direction.normalized()
+	
+	$dodge_time.start()
+	#when the timer ends, the below loop while stop (uncompleted)
+	while true:
+		velocity=dodge_direction * dodge_speed
+	
+	print(velocity)
+	
+	is_dodging=false
+	
+	
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	
